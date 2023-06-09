019d7870ba702c62508b644e9efb4402a14fc4de
Madhav Gautam <63944499+mxdhavgautam@users.noreply.github.com>
2023-06-09 10:35:33 +0530

    Updated Dependencies

--- lib/widgets/frame_title.dart
+++ lib/widgets/frame_title.dart
@@ -1,4 +1,4 @@
-import 'package:dileepabandara_dev/theme/responsive_screen_provider.dart';
+import 'package:mxdhavgautam/theme/responsive_screen_provider.dart';
 import 'package:flutter/material.dart';
 
 class FrameTitle extends StatelessWidget {
